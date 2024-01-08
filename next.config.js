275e9f405f485d63e8c819e22b9ac4310ba20f28
false0x <155391869+false0x@users.noreply.github.com>
2024-01-08 08:47:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `https://apis.na4u.ru/api/:path*`
-      }
-    ]
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
