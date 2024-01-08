e1be260eb63bf0ad2995d4f9f9127556f94759ff
false0x <155391869+false0x@users.noreply.github.com>
2024-01-08 08:22:15 +0300

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
-export const API_URL = `/api`
+export const API_URL = `/api/v1`
 
 export const getAuthUrl = (path: string = '') => `/auth${path}`
 export const getArticleUrl = (path: string = '') => `/article${path}`
